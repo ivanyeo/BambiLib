c7bf4e5f60048e9618f0a8513d88662293d47b39
Ivan Yeo <ivanyeo@ucla.edu>
2014-05-25 01:14:08 -0700

    Initial commit.

---
+++ src/com/ndnxr/bambi/BambiLib.java
@@ -0,0 +1,15 @@
+package com.ndnxr.bambi;
+
+public class BambiLib {
+
+	// Messages to be passed to the Service by Clients for processing
+	static final int MESSAGE_REGISTER_CLIENT 	= 1;
+	static final int MESSAGE_UNREGISTER_CLIENT 	= 2;
+	static final int MESSAGE_REGISTER_SUCCESS	= 3;
+	static final int MESSAGE_UNREGISTER_SUCCESS	= 4;
+	
+	// Supported Service Functions
+	static final int MESSAGE_SEND_EMAIL			= 11;
+//	static final int MESSAGE_REGISTER_SUCCESS	= 3;
+
+}
