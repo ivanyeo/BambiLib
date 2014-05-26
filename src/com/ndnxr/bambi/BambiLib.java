155f261505178af07d628996fd6cdc749a49b0df
Ivan Yeo <ivanyeo@ucla.edu>
2014-05-26 04:56:18 -0700

    Update BambiLib with wire frame function of sendEmail().

--- src/com/ndnxr/bambi/BambiLib.java
+++ src/com/ndnxr/bambi/BambiLib.java
@@ -1,7 +1,6 @@
 package com.ndnxr.bambi;
 
 public class BambiLib {
-
 	// Messages to be passed to the Service by Clients for processing
 	static final int MESSAGE_REGISTER_CLIENT 	= 1;
 	static final int MESSAGE_UNREGISTER_CLIENT 	= 2;
@@ -10,6 +9,28 @@ public class BambiLib {
 	
 	// Supported Service Functions
 	static final int MESSAGE_SEND_EMAIL			= 11;
-//	static final int MESSAGE_REGISTER_SUCCESS	= 3;
-
+	
+	// Task Information Functions
+	static final int MESSAGE_GET_BAMBI_TASKS		= 21;
+	static final int MESSAGE_RECEIVE_BAMBI_TASKS	= 22;
+	
+	// Urgency Enum
+	public enum URGENCY {
+		NOW, SCHEDULE, WIFI_ACTIVE,
+	}
+	
+	// Task Type Enum
+	public enum TASK_TYPE {
+		EMAIL
+	}
+	
+	/**
+	 * Sends a requested email as a Task to BambiService.
+	 * 
+	 * @return true on success; false otherwise.
+	 */
+	public static boolean sendEmail(Task task, Email email) {
+		// TODO Write the email task
+		return false;
+	}
 }
