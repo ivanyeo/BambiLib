4602cbc7c2e1bd26cc686e76639efa270afeff5d
Ivan Yeo <ivanyeo@ucla.edu>
2014-05-27 03:37:14 -0700

    Change Email to implement Parcelable instead of the slower Serializable.

--- src/com/ndnxr/bambi/Email.java
+++ src/com/ndnxr/bambi/Email.java
@@ -1,12 +1,14 @@
 package com.ndnxr.bambi;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Email implements Serializable {
+public class Email implements Parcelable {
 	private String to, from, cc, bcc, subject, message;
-	private String[] filePaths;
+	private String[] filePaths = new String[1];
 
-	private static final long serialVersionUID = 0L;
+	// For Serializable interface
+	//private static final long serialVersionUID = 0L;
 
 	public Email(String to, String from, String cc, String bcc, String subject,
 			String message, String[] filePaths) {
@@ -36,6 +38,16 @@ public class Email implements Serializable {
 		this.from = from;
 		this.subject = subject;
 		this.message = message;
+	}
+
+	public Email(Parcel in) {
+		to = in.readString();
+		from = in.readString();
+		cc = in.readString();
+		bcc = in.readString();
+		subject = in.readString();
+		message = in.readString();
+		in.readStringArray(filePaths);
 	}
 
 	public String getCc() {
@@ -93,4 +105,35 @@ public class Email implements Serializable {
 	public void setFilePaths(String[] filePaths) {
 		this.filePaths = filePaths;
 	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s, %s, %s, %s", to, from, subject, message);
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(to);
+		dest.writeString(from);
+		dest.writeString(cc);
+		dest.writeString(bcc);
+		dest.writeString(subject);
+		dest.writeString(message);
+		dest.writeStringArray(filePaths);
+	}
+	
+	public static final Parcelable.Creator<Email> CREATOR = new Parcelable.Creator<Email>() {
+		public Email createFromParcel(Parcel in) {
+			return new Email(in);
+		}
+
+		public Email[] newArray(int size) {
+			return new Email[size];
+		}
+	};
 }
